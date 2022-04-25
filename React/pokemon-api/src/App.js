5dc1c769dbe84a8c27112fca0f09b17910eedebf
Benton-Michael <mbenton002@gmail.com>
2022-04-25 14:25:24 -0600

    Pokemon API practice assignment

--- React/pokemon-api/src/App.js
+++ React/pokemon-api/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  // We expect an array of pokemon objects back and so we will
+  //    choose an empty array as the initial value to be held
+  //    in state
+  const [pokemon, setPokemon] = useState([]);
+
+  // this will run immediately after the JSX is rendered
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((response) => {
+        // this will return the data from the response object
+        //    in a json format
+        return response.json();
+      })
+      .then((response) => {
+        // store the json converted data in state so it can be displayed
+        setPokemon(response.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // no clean up statement is required since we are not using resources
+    //    that will continue to run
+  }, []);
+  // The empty array as the second parameter makes it so that useEffect
+  //    does NOT run more than once
+
+  // using an unordered list to match the wireframe
+  // wireframe is just a bullet point list of names
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: "200px", margin: "auto" }}>
+      <ul>
+        {pokemon.map((pokemonObj, index) => {
+          return <li key={index}> {pokemonObj.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
