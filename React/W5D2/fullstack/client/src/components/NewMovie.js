74c7c941597b2b1dbe416c5b478e0b0a2ef06c18
Benton-Michael <mbenton002@gmail.com>
2022-05-11 15:43:19 -0600

    completed the front-end form necessary to add new movies

--- React/W5D2/fullstack/client/src/components/NewMovie.js
+++ React/W5D2/fullstack/client/src/components/NewMovie.js
@@ -17,19 +17,89 @@ function NewMovie() {
   const navigate = useNavigate();
   const submitHandler = (e) => {
     e.preventDefault();
-    axios.post("http://localhost:5001/api/movies", {
-      // key and value are the same so no colon needed in syntax
-      title,
-      genre,
-      boxArt,
-      watchLength,
-      rating,
-      actors,
-      isKidFriendly,
-      releaseYear,
-    });
+    axios
+      .post("http://localhost:5001/api/movies", {
+        // key and value are the same so no colon needed in syntax
+        title,
+        genre,
+        boxArt,
+        watchLength,
+        rating,
+        actors,
+        isKidFriendly,
+        releaseYear,
+      })
+      .then((res) => {
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
   };
-  return <div>NewMovie</div>;
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <select onChange={(e) => setGenre(e.target.value)}>
+        <option>Select a Genre</option>
+        <option value="Comedy">Comedy</option>
+        <option value="Fantasy">Fantasy</option>
+        <option value="Drama">Drama</option>
+        <option value="Sci-Fi">Sci-Fi</option>
+        <option value="Horror">Horror</option>
+        <option value="Documentary">Documentary</option>
+        <option value="Action">Action</option>
+        <option value="Animated">Animated</option>
+      </select>
+      <label>Image Link</label>
+      <input
+        type="text"
+        value={boxArt}
+        onChange={(e) => setBoxArt(e.target.value)}
+      />
+      <label>Watch Length</label>
+      <input
+        type="text"
+        value={watchLength}
+        onChange={(e) => setWatchLength(e.target.value)}
+      />
+      <label>Rating</label>
+      <select onChange={(e) => setRating(e.target.value)}>
+        <option></option>
+        <option value="G">G</option>
+        <option value="PG">PG</option>
+        <option value="PG-13">PG-13</option>
+        <option value="R">R</option>
+        <option value="NC-17">NC-17</option>
+      </select>
+      {/* We can't take in user input as an array */}
+      {/* so we'll accept the user input as a string with a commma between the actors */}
+      {/* We're using split() on the comma - this will split the names into an array */}
+      <lable>Actors</lable>
+      <input
+        type="text"
+        value={actors}
+        onChange={(e) => setActors(e.target.value.split(","))}
+      />
+      <label>Release Year</label>
+      <input
+        type="text"
+        value={releaseYear}
+        onChange={(e) => setReleaseYear(e.target.value)}
+      />
+      <label>isKidFriendly</label>
+      <input
+        type="checkbox"
+        checked={isKidFriendly}
+        onChange={(e) => setIsKidFriendly(e.target.checked)}
+      />
+
+      <input type="submit" value="Add Movie" />
+    </form>
+  );
 }
 
 export default NewMovie;
